0b93a0b549c4f0b3bfaf665b797ac7a62808249d
David Drvar <david.drvar.bogdanovic@gmail.com>
2020-05-23 14:50:13 +0200

    Renovation CRUD

---
+++ code/Controller/abstract/IRenovationController.cs
@@ -0,0 +1,13 @@
+﻿using Controller;
+using Model.Director;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace bolnica.Controller
+{
+    public interface IRenovationController : IController<Renovation,long>
+    {
+    }
+}
